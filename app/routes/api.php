83898ed4f108522b8469553bb6aa4d08dadebf2d
Gieandes Silva <contato@gieandessilva.com>
2019-07-23 09:57:10 -0300

    * Adicionadas as rotas para os tweets e users

--- app/routes/api.php
+++ app/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/most_relevants', function (Request $request) {
+    return 'tweets relevantes';
+});
+
+Route::middleware('auth:api')->get('/most_mentions', function (Request $request) {
+    return 'users mencionados';
+});
