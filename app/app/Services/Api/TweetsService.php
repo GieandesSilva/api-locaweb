a3fa066b0c3fc4d70a935af7306320175b51ba05
Gieandes Silva <contato@gieandessilva.com>
2019-07-24 09:45:36 -0300

    * Adicionada a ordenacão no servico

--- app/app/Services/Api/TweetsService.php
+++ app/app/Services/Api/TweetsService.php
@@ -26,19 +26,23 @@ class TweetsService
     public function execute()
     {
         try {
-            $tweets = array();
+            $tweets = collect();
+
             $response = $this->client->get($this->host, [
                 'headers'        => ['Username' => $this->username],
             ]);
 
             $allTweets = json_decode($response->getBody()->getContents());
+
             foreach ($allTweets->statuses as $tweet) {
                 if($tweet->in_reply_to_user_id_str != 42 && (sizeof($tweet->entities->user_mentions) > 0 && $tweet->entities->user_mentions[0]->id == 42)) {
-                    array_push($tweets, $tweet);
+                    $tweets->push($tweet);
                 }
             }
-            return $tweets;
+
+            return $tweets->values()->sortByDesc('favorite_count')->sortByDesc('retweet_count')->sortByDesc('user.followers_count');
         }catch (Exception $e) {
+
              Log::info('[TWEETS][SERVICE]: ' . $e->getMessage());
              abort(502, 'O serviço está indisponível, que chato :(, nossos profissionais estão se dedicando para solucionar');
         }
