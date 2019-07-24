cfcd91cc06462b43f6ba867608490b2d5001e549
Gieandes Silva <contato@gieandessilva.com>
2019-07-24 09:38:50 -0300

    * Alteracão na disposicão dos campos dos tweets e remocão do tipo do retorno

--- app/app/Transformers/TweetsTransformer.php
+++ app/app/Transformers/TweetsTransformer.php
@@ -7,17 +7,17 @@ namespace App\Transformers;
 class TweetsTransformer
 {
 
-    public function transform($tweet) : array
+    public function transform($tweet)
     {
         return [
-            'followers_count' => $tweet->user->followers_count,
             'screen_name' => $tweet->user->screen_name,
-            'profile_link' => 'https://twitter.com/' . $tweet->user->screen_name,
-            'created_at' => $tweet->created_at,
-            'link' => 'https://twitter.com/' . $tweet->user->screen_name . '/status/'. $tweet->id_str,
+            'followers_count' => $tweet->user->followers_count,
             'retweet_count' => $tweet->retweet_count,
+            'favorite_count' => $tweet->favorite_count,
             'text' => $tweet->text,
-            'favorite_count' => $tweet->favorite_count
+            'created_at' => $tweet->created_at,
+            'profile_link' => 'https://twitter.com/' . $tweet->user->screen_name,
+            'link' => 'https://twitter.com/' . $tweet->user->screen_name . '/status/'. $tweet->id_str
         ];
     }
 }
