3d33a266392249d12995bb35775412a530261a1e
Gieandes Silva <contato@gieandessilva.com>
2019-07-24 09:39:07 -0300

    * Alteracão na disposicão dos campos dos usuários e remocão do tipo do retorno

--- app/app/Transformers/UsersTransformer.php
+++ app/app/Transformers/UsersTransformer.php
@@ -7,19 +7,19 @@ namespace App\Transformers;
 class UsersTransformer
 {
 
-    public function transform($tweet) : array
+    public function transform($tweet)
     {
         return [
             $tweet->user->screen_name => [
                 [
-                    'created_at' => $tweet->created_at,
-                    'profile_link' => 'https://twitter.com/' . $tweet->user->screen_name,
-                    'favorite_count' => $tweet->favorite_count,
                     'screen_name' => $tweet->user->screen_name,
                     'followers_count' => $tweet->user->followers_count,
-                    'link' => 'https://twitter.com/' . $tweet->user->screen_name . '/status/'. $tweet->id_str,
+                    'retweet_count' => $tweet->retweet_count,
+                    'favorite_count' => $tweet->favorite_count,
                     'text' => $tweet->text,
-                    'retweet_count' => $tweet->retweet_count
+                    'created_at' => $tweet->created_at,
+                    'profile_link' => 'https://twitter.com/' . $tweet->user->screen_name,
+                    'link' => 'https://twitter.com/' . $tweet->user->screen_name . '/status/'. $tweet->id_str
                 ]
             ]
         ];
